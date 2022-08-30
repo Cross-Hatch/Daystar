f267e3b4f742e91c59a1264642745930e0a40a62
Ashiangmor <56304597+Kuameh@users.noreply.github.com>
2022-08-30 13:52:04 +0000

    misc👀: whitespace formatting

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,12 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
 import Header from "../Header/Header"
 import Footer from "../Footer/Footer"
 import { GlobalStyle } from "../GlobalStyles"
 import { ThemeProvider } from "styled-components"
 
-const  Layout = ({ children }) => {
+const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
