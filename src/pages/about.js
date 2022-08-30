b01118b39f5dddaf19c0f3d4c8dad5ff834c6f80
Ashiangmor <56304597+Kuameh@users.noreply.github.com>
2022-08-30 13:58:49 +0000

    feat✨: added about page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Layout from "../components/Layout/Layout";
+
+const AboutPage = () => {
+    return ( 
+        <Layout>
+            About Page
+        </Layout>
+     );
+}
+ 
+export default AboutPage;
