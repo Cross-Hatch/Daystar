b434d72db9c966ae5063f1529c4abc8ae3d85b7d
Ashiangmor <56304597+Kuameh@users.noreply.github.com>
2022-08-30 13:25:39 +0000

    feat✨: styling for header container

--- src/components/Header/Header.style.js
+++ src/components/Header/Header.style.js
@@ -3,11 +3,15 @@ import styled from "styled-components";
 
 export const HeaderContainer = styled.header`
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
-  padding: 1em 2em;
+  padding-block: 1.2em;
+  padding-inline: clamp(2em, 5em, 500em);
   min-width: 100%;
-  height: 3em;
+  margin: 0 0 1em 0;
+  height: 4em;
+  border-radius: 0 0 20px 20px;
+  background-color: yellow;
 `
 
 export const LogoImg = styled(GatsbyImage)`
