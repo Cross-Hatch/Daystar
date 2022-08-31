5503c28c428a7410cd8867f3961d6c86d87932f7
rhekord <soanum001@st.ug.edu.gh>
2022-08-31 19:57:11 +0000

    Modified index.js
    -wrapped the whole page in a div and added a background to the page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,17 @@ import * as React from "react"
 
 import Layout from "../components/Layout/Layout"
 import Seo from "../components/SEO/seo"
-import ProductCard from "../components/ProductCard/ProductCard.js"
+import { Wrapper } from "../Styles/index.styles"
+// import ProductCard from "../components/ProductCard/ProductCard.js"
 
 const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <ProductCard data={{title: "ginger lime", description: "ginger lemon drink"}} />
-   Index
-  </Layout>
+  <Wrapper>
+    <Layout>
+      <Seo title="Home" />
+      {/* <ProductCard data={{title: "ginger lime", description: "ginger lemon drink"}} /> */}
+      Index
+    </Layout>
+  </Wrapper>
 )
 
 export const Head = () => <Seo title="Home" />
