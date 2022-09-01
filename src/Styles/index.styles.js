4efd06fb61afadaf055f6df2eb980005fe410515
rhekord <soanum001@st.ug.edu.gh>
2022-09-01 15:59:31 +0000

    Added index.styles
    -added styles to background lines

--- src/Styles/index.styles.js
+++ src/Styles/index.styles.js
@@ -1,7 +1,21 @@
 import styled from "styled-components"
 
-export const Wrapper = styled.div`
+export const PageWrapper = styled.div`
   background-color: yellow;
   min-height: 100vh;
   width: 100%;
+`
+//wraps inner elements of the index page
+export const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+
+  .wrapper {
+    width: 100%;
+    height: 100%;
+  }
+
+  .bg-lines {
+    opacity: 0.2 !important;
+  }
 `
