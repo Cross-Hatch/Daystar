8175c26f2ce713be4b85c1ed6c7c365c9c380056
rhekord <soanum001@st.ug.edu.gh>
2022-09-04 17:49:41 +0000

    Mordified Footer.style.js
    -Added background-color and height to the footer container

--- src/components/Footer/Footer.style.js
+++ src/components/Footer/Footer.style.js
@@ -1,4 +1,6 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
 export const FooterContainer = styled.footer`
+  background-color: ${props => props.theme.bg};
+  height: 4em;
 `
