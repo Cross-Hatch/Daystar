71e037a08960fffa5ae1f2cb3a3cc68e78c69f08
rhekord <soanum001@st.ug.edu.gh>
2022-09-08 20:25:54 +0000

    Modified About.js
    -Added heading,text and button components

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,45 @@
-import * as React from "react";
-import Layout from "../components/Layout/Layout";
+import * as React from "react"
+import Layout from "../components/Layout/Layout"
+import { Wrapper } from "../styles/about.styles"
+// import { defaultTheme } from "../components/ThemeProvider"
 
 const AboutPage = () => {
-    return ( 
-        <Layout>
-            About Page
-        </Layout>
-     );
+  return (
+    <Layout>
+      <Wrapper>
+        <h1>WHO WE ARE</h1>
+        <div className="text-container">
+          lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla lorem
+          ipsum dolor sit amet, consectetur adipiscing elit. Nulla lorem2000
+          ipsum dolor sit amet, consectetur adipiscing elit. Nulla lorem2000
+          ipsum dolor sit amet, consectetur adipiscing elit. Nulla lorem ipsum
+          dolor sit amet, consectetur adipiscing elit. Nulla lorem ipsum dolor
+          sit amet, consectetur adipiscing elit. Nulla lorem2000 ipsum dolor sit
+          amet, consectetur adipiscing elit.lorem ipsum dolor sit amet,
+          consectetur adipiscing elit. Nulla lorem ipsum dolor sit amet,
+          consectetur adipiscing elit. Nulla lorem2000 ipsum dolor sit amet,
+          consectetur adipiscing elit. Nulla lorem2000 ipsum dolor sit amet,
+          consectetur adipiscing elit. Nulla lorem ipsum dolor sit amet,
+          consectetur adipiscing elit. Nulla lorem ipsum dolor sit amet,
+        </div>
+        <div className="text-container">
+          lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla lorem
+          ipsum dolor sit amet, consectetur adipiscing elit. Nulla lorem2000
+          ipsum dolor sit amet, consectetur adipiscing elit. Nulla lorem2000
+          ipsum dolor sit amet, consectetur adipiscing elit. Nulla lorem ipsum
+          dolor sit amet, consectetur adipiscing elit. Nulla lorem ipsum dolor
+          sit amet, consectetur adipiscing elit. Nulla lorem2000 ipsum dolor sit
+          amet, consectetur adipiscing elit.lorem ipsum dolor sit amet,
+          consectetur adipiscing elit. Nulla lorem ipsum dolor sit amet,
+          consectetur adipiscing elit. Nulla lorem2000 ipsum dolor sit amet,
+          consectetur adipiscing elit. Nulla lorem2000 ipsum dolor sit amet,
+          consectetur adipiscing elit. Nulla lorem ipsum dolor sit amet,
+          consectetur adipiscing elit. Nulla lorem ipsum dolor sit amet,
+        </div>
+        <button className="Contactus-button">CONTACT US</button>
+      </Wrapper>
+    </Layout>
+  )
 }
- 
-export default AboutPage;
+
+export default AboutPage
