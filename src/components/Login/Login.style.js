f15607c96a6f18e75b42ecde65159ea4b69f298f
GideonAg <gideonagbosu72@gmail.com>
2022-09-09 11:48:36 +0000

    Modified to be responsive on smaller screens

--- src/components/Login/Login.style.js
+++ src/components/Login/Login.style.js
@@ -4,28 +4,42 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-around;
-  margin-top: 5em;
+  margin: 5em 0;
   text-align: center;
   height: 80vh;
   padding: 5em;
+  font-weight: bold;
 
-  @media screen and (max-width: 800px) {
+  @media screen and (max-width: 820px) {
     flex-direction: column;
     min-height: 60em;
+    margin-top: -2em;
+    margin-bottom: -2em;
+    height: 100vh;
+    font-weight: lighter;
   }
 `
 
 export const WelcomeBox = styled.div`
   padding: 3em;
   height: 30em;
+
+  @media screen and (max-width: 820px) {
+    margin-bottom: 1em;
+    height: auto;
+  }
 `
 
 export const WelcomeText = styled.div`
-  font-size: 2.3em;
+  font-size: 2.7em;
   font-weight: bolder;
   display: flex;
   align-items: center;
   justify-content: center;
+
+  @media screen and (max-width: 820px) {
+    font-size: 1.6em;
+  }
 `
 
 export const LoginText = styled.div`
@@ -33,9 +47,8 @@ export const LoginText = styled.div`
   align-items: center;
   justify-content: center;
   align-items: center;
+  padding: 1em;
 `
-
-export const ProductsImage = styled.img``
 
 export const FormContainer = styled.div`
   height: 21em;
@@ -52,6 +65,7 @@ export const FormInput = styled.input`
   display: block;
   width: 18em;
   border: none;
+  border-radius: 0.2em;
   background-color: lightgray;
 `
 
@@ -70,6 +84,7 @@ export const FormButton = styled.button`
 export const SignUpText = styled.text`
   display: block;
   margin: 2em;
+  margin-bottom: -1em;
 `
 
 export const Form = styled.form``
