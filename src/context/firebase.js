7411339c426aad03ed542eff935f40077c803d6d
David Boateng <davebostons2017@gmail.com>
2022-09-09 21:32:28 +0000

    created firebase.js

---
+++ src/context/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBlK2GAMl1pn5HHNRr47tP2LcZLeZAqyXo",
+  authDomain: "daystar-b9a30.firebaseapp.com",
+  projectId: "daystar-b9a30",
+  storageBucket: "daystar-b9a30.appspot.com",
+  messagingSenderId: "394586632743",
+  appId: "1:394586632743:web:9079bff162853500aa1402"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
