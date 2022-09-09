373bc2eda4ca27b84dcd329c0219654094521eed
GideonAg <gideonagbosu72@gmail.com>
2022-09-09 02:19:44 +0000

    Added login.js file

---
+++ src/pages/login.js
@@ -0,0 +1,42 @@
+import * as React from "react"
+import Layout from "../components/Layout/Layout"
+import {
+  Container,
+  WelcomeBox,
+  FormContainer,
+  WelcomeText,
+  LoginText,
+  ProductsImage,
+  Form,
+  FormInput,
+  FormButton,
+  SignUpText,
+} from "../components/Login/Login.style"
+
+const Login = () => {
+  return (
+    <Layout>
+      <Container>
+        <WelcomeBox>
+          <WelcomeText>Welcome Back</WelcomeText>
+          <LoginText>
+            Login into your Daystar account to make purchases
+          </LoginText>
+          <ProductsImage src="../images/productsImage.png" alt="image" />
+        </WelcomeBox>
+        <FormContainer>
+          <Form>
+            <FormInput placeholder="EMAIL OR PHONE" />
+            <FormInput placeholder="PASSWORD" />
+            <FormButton>LOGIN</FormButton>
+            <SignUpText>
+              Don't have an account? <b>Sign up.</b>
+            </SignUpText>
+          </Form>
+        </FormContainer>
+      </Container>
+    </Layout>
+  )
+}
+
+export default Login
